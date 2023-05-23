61dde4a6bfa24d378c0445450ab82089a856baf2
Henrygoalrepo <herygoalrepo132@gmail.com>
2023-05-23 17:16:55 +0530

    minimum stability chnahed

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'chatbots/chatbotblueprint',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8e5054bf14f66482686333c18ceb40a0b791b0d5',
+        'type' => 'composer-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'chatbots/chatbotblueprint' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8e5054bf14f66482686333c18ceb40a0b791b0d5',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
