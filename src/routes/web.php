28cf40679c18d621850d7e3e5b6a8caf2d43d191
Henrygoalrepo <herygoalrepo132@gmail.com>
2023-05-30 16:27:15 +0530

    error_start

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,7 @@
 use Illuminate\Support\Facades\Route;
 use ChatBot\Blueprint\Http\Controllers\ChatBotController;
 use ChatBot\Blueprint\Http\Controllers\Chatbot\Coversationflow;
-Route::get('/chatbot',[ChatBotController::class,'bot'])->name('chatbot.bots');
+
 
 
 Route::resource('conversationflow',Coversationflow::class);
