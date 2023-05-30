857358c11eebe66e0fb2a7823762d29717544fae
Henrygoalrepo <herygoalrepo132@gmail.com>
2023-05-30 16:11:12 +0530

    desired_filename

--- src/Traits/PublishMigration.php
+++ src/Traits/PublishMigration.php
@@ -8,7 +8,9 @@ trait PublishMigration{
          if($this->app->runningInConsole()){
             $geneartor=function(string $directoty):Generator{
                 foreach($this->app()->make('files')->allFiles($directory) as $file){
-                    yield $file->getPathName()=>$this->app->databasePath('migrations/'.now()->format('y_m_d_His').Str::after($file->getFileName(),'0000_00_00_000000'));
+                    $file_name='_'.$file->getFileName();
+                    $mod_file=preg_replace('/_[0-9]+/','',$file_name);
+                    yield $file->getPathName()=>$this->app->databasePath('migrations/'.now()->format('y_m_d_His').$mod_file);
                 }
             };
             $this->publishes(iterator_to_array($geneartor($directory)),'migrations');
