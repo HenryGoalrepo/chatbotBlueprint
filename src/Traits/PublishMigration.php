f54fb0587100e7c2b3f94f4df36fe2c5f68ff6bd
Henrygoalrepo <herygoalrepo132@gmail.com>
2023-05-30 15:19:24 +0530

    not updating

---
+++ src/Traits/PublishMigration.php
@@ -0,0 +1,21 @@
+<?php
+namespace ChatBot\Blueprint\Traits;
+use Generator;
+
+trait PublishMigration{
+
+    public function registerMigration($directory):void{
+         if($this->app->runningInConsole()){
+            $geneartor=function(string $directoty):Generator{
+                foreach($this->app()->make('files')->allFiles($directory) as $file){
+                    yield $file->getPathName()=>$this->app->databasePath('migrations/'.now()->format('y_m_d_His').Str::after($file->getFileName(),'0000_00_00_000000'));
+                }
+            };
+            $this->publishes(iterator_to_array($geneartor($directory)),'migrations');
+         }
+    }
+}
+
+
+
+?>
