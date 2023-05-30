28a8e9094435a427ea986946742419d22386b254
Henrygoalrepo <herygoalrepo132@gmail.com>
2023-05-30 15:31:11 +0530

    migration_tarit

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,8 @@
 use Illuminate\Support\Facades\Route;
 use ChatBot\Blueprint\Http\Controllers\ChatBotController;
 use ChatBot\Blueprint\Http\Controllers\Chatbot\Coversationflow;
+Route::get('/chatbot',[ChatBotController::class,'bot'])->name('chatbot.bots');
+
 
 Route::resource('conversationflow',Coversationflow::class);
 
